6bece00d132733bebe71ba60da1a1d1efccbca51
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-13 21:38:46 -0300

    Incluído os atributos de endereço

--- src/app/models/Pessoa.ts
+++ src/app/models/Pessoa.ts
@@ -1,6 +1,12 @@
 export class Pessoa{
     id: number = 0
     nome: string = ''
+    cpf: number = 0
     sexo: string = ''
+    cep : number = 0
+    ruaLogradoro: string = ''
+    bairro: string =''
+    cidade: string = ''
+    uf: string = ''
 
 }
