aa82ec45d3e68acbcfe562504d21a7b07212d2c2
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-13 21:38:11 -0300

    Criado funções para salvar e limpar

--- src/app/component/atleta-component/atleta-component.ts
+++ src/app/component/atleta-component/atleta-component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AtletaService } from '../../service/atleta-service';
+import { Pessoa } from '../../models/Pessoa';
 
 @Component({
   selector: 'app-atleta-component',
@@ -10,17 +12,52 @@ import { FormsModule } from '@angular/forms';
 export class AtletaComponent {
   //DELCARAÇÃO DOS ATRIBUTOS DO COMPONENTE
   nome = ''
-  cpf = ''
+  cpf = 0
   sexo = ''
-  cep = ''
+  cep = 0
   ruaLogradouro = ''
   bairro = ''
   cidade = ''
   uf = ''
 
+  //DECLARAÇÃO DO CONSTRUTOR  
+  constructor(private atletaService: AtletaService) { }
+
   //DECLARAÇÃO DE FUNÇÕES
-  exibeDados(){
-    console.log(this.nome, this.cpf, this.sexo, this.ruaLogradouro, this.bairro, this. cidade, this.uf)
+  exibeDados() {
+    console.log(this.nome, this.cpf, this.sexo, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
   }
+
+  salvarAtleta() {
+    const pessoaAtleta = new Pessoa()
+    pessoaAtleta.nome = this.nome
+    pessoaAtleta.cpf = this.cpf
+    pessoaAtleta.sexo = this.sexo
+    pessoaAtleta.cep = this.cep
+    pessoaAtleta.ruaLogradoro = this.ruaLogradouro
+    pessoaAtleta.bairro = this.bairro
+    pessoaAtleta.cidade = this.cidade
+    pessoaAtleta.uf = this.uf
+
+    this.atletaService.adicionar(pessoaAtleta)
+
+    this.atletaService.listar()
+
+    this.limparAtributos()
+
+  }
+
+  limparAtributos() {
+    this.nome = ''
+    this.cpf = 0
+    this.sexo = ''
+    this.cep = 0
+    this.ruaLogradouro = ''
+    this.bairro = ''
+    this.cidade = ''
+    this.uf = ''
+  }
+
+
 
 }
