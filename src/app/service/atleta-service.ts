c52049801a4bd2365f756028219dac22be2b1272
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-14 21:42:18 -0300

    Criado as funções crud com  a API mockapi

--- src/app/service/atleta-service.ts
+++ src/app/service/atleta-service.ts
@@ -1,11 +1,51 @@
 import { Injectable } from '@angular/core';
-
-import { Pessoa } from '../models/Pessoa';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Atleta } from '../models/Atleta';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AtletaService {
+  //DECLARAÇÃO CONSTRUTOR
+  constructor(private http: HttpClient) { }
+
+  //ADICIONAR NA API
+  adicionarAtleta(atleta: Atleta): Observable<Atleta> {
+    const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
+
+    return this.http.post<Atleta>(urlApi, atleta)
+  }
+
+  //LISTAR ATLETAS NA API
+  listarAtletas(): Observable<Atleta[]> {
+    const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
+
+    return this.http.get<Atleta[]>(urlApi)
+  }
+
+  //LISTAR ATLETA
+  listarAtleta(idAtleta: number):Observable<Atleta>{
+    const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
+
+    return this.http.get<Atleta>(urlApi)
+  }
+
+  //EXCLUIR NA API
+  exluirAtleta(atleta: Atleta): Observable<Atleta> {
+    const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
+
+    return this.http.delete<Atleta>(urlApi)
+  }
+
+  //ALTERAR NA API
+  alterarAtleta(atleta: Atleta):Observable<Atleta>{
+    const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
+
+    return this.http.put<Atleta>(urlApi, atleta)
+  }
+
+  /*
   private atletas: Pessoa[] = []
 
   adicionar(pessoa: Pessoa) {
@@ -39,6 +79,6 @@ export class AtletaService {
       this.atletas[posArray] = pessoa
     }
 
-  }
+  }*/
 
 }
