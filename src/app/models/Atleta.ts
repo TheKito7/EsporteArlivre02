df78e4360e1b7b3625c877bdf6bbf2f7d120e672
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-14 21:36:36 -0300

    Criado o Modelo Atleta

---
+++ src/app/models/Atleta.ts
@@ -0,0 +1,12 @@
+export class Atleta{
+    id: number = 0
+    nome: string = ''
+    cpf: number = 0
+    sexo: string = ''
+    cep : number = 0
+    ruaLogradoro: string = ''
+    bairro: string =''
+    cidade: string = ''
+    uf: string = ''
+
+}
