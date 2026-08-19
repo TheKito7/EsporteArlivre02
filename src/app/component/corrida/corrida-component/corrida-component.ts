07339a6e4558aeb5c14bccaef48a85e4f5148a5f
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-19 20:25:30 -0300

    Criado a função enviar dados

--- src/app/component/corrida/corrida-component/corrida-component.ts
+++ src/app/component/corrida/corrida-component/corrida-component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Corrida } from '../../../models/Corrida';
 
 @Component({
   selector: 'app-corrida-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './corrida-component.html',
   styleUrl: './corrida-component.css',
 })
-export class CorridaComponent {}
+export class CorridaComponent {
+  //DEFININDO OS ATRIBUTOS DO COMPONENTE CorridaComponent
+  id = 0
+  descricao_corrida = ''
+  data_corrida = ''
+  distancia5km = false
+  distancia10km = false
+  distancia25km = false
+
+  //FUNÇÃO PARA CADASTRAR E ALTERAR
+  dadosFormulario(){
+    const corrida = new Corrida()
+    corrida.descricao_corrida = this.descricao_corrida
+    corrida.data_corrida =  this.data_corrida
+    corrida.distancia5km = this.distancia5km
+    corrida.distancia10km = this.distancia10km
+    corrida.distancia25km = this.distancia25km
+
+    
+
+
+    
+  }
+
+}
